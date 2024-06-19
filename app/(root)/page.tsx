4ede71ccaae6e6cad6ee158cc703c9ba94a6655f
mr-nobody-7 <vivekanandagodi@gmail.com>
2024-06-19 07:44:43 +0530

    added podcast card and fake date

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,23 @@
+import PodcastCard from "@/components/PodcastCard";
+import { podcastData } from "@/constants";
 import React from "react";
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
-        <h1 className="text-20 font-bold text-white-1">Trending podcasts</h1>
+        <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+        <div className="podcast_grid">
+          {podcastData.map(({ imgURL, title, description, id }) => (
+            <PodcastCard
+              key={id}
+              imgUrl={imgURL}
+              title={title}
+              description={description}
+              podcastId={id}
+            />
+          ))}
+        </div>
       </section>
     </div>
   );
