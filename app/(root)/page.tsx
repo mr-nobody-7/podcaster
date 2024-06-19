aeffb7abd072571091512a68aac5d45a3841e82c
mr-nobody-7 <vivekanandagodi@gmail.com>
2024-06-19 08:33:30 +0530

    setup convex and created schema

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,14 @@
+"use client";
+import React from "react";
 import PodcastCard from "@/components/PodcastCard";
 import { podcastData } from "@/constants";
-import React from "react";
 
 const Home = () => {
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
         <h1 className="text-20 font-bold text-white-1">Trending Podcasts</h1>
+
         <div className="podcast_grid">
           {podcastData.map(({ imgURL, title, description, id }) => (
             <PodcastCard
